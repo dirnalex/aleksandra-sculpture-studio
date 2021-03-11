5ab9d0dba5b02990ce1c8c09038c50e6707fab3a
Aleksei Dyrnaev <dirnalex@gmail.com>
2021-03-11 20:57:44 +0100

    Remove 3d

--- src/pages/LanguageSelectPage.js
+++ src/pages/LanguageSelectPage.js
@@ -1,20 +1,15 @@
-import React, {Fragment, Suspense} from 'react';
+import React, {Fragment} from 'react';
 import {Link} from 'react-router-dom';
 
 import {LanguageSelectBox} from './LanguageSelectPageStyles';
 import Logo from '../components/icons/Logo';
-import {useLoader} from 'react-three-fiber';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 import useExtendedRouteMatch from '../hooks/useExtendedRouteMatch';
 
 const LanguageSelectPage = () => {
   const {urlWithoutSlash} = useExtendedRouteMatch();
-  const gltf = useLoader(GLTFLoader, '/autoportret.glb')
   return (
     <Fragment>
-      <Suspense fallback={<Logo/>}>
-        <primitive object={gltf.scene}/>
-      </Suspense>
+      <Logo/>
       <LanguageSelectBox>
         <Link to={`${urlWithoutSlash}/en`}>en</Link>
         <Link to={`${urlWithoutSlash}/pl`}>pl</Link>
